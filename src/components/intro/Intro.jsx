4dc3e39280c43016cfd48aa004f90c9523062f1d
Bojan Golic <bokigolic32@gmail.com>
2022-07-06 11:41:16 -0400

    intro done, add name, title add picture

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,39 +1,32 @@
-import React from 'react'
-import "./intro.scss"
-import {init} from 'ityped'
-import { useEffect, useRef } from "react";
+import "./intro.scss";
+
 
 export default function Intro() {
-  const textRef = useRef();
-  useEffect(() => {
-    init(textRef.current, {
-      showCursor: true,
-      backDelay: 1500,
-      backSpeed:60,
-      strings: ["Developer", "Designer", "Content Creator"],
-    });
-  }, []);
+ 
+
   return (
-      <div className="intro" id="intro">
-          <div className="left">
-            <div className="imgContainer">
-              <img src="assets/man.png" alt="" />
-            </div>
-          </div>
-
-      
-        <div className='right'>
-          <div className='wrapper'>
-            <h2>Hi There, I'm </h2>
-            <h1>Bojan Golic</h1>
-            <h3>Fullstack <span> Developer</span></h3>
-
-          </div>
-          <a href='#portfolio'>
-            <img src='assets/down.png' alt='arrow' />
-          </a>
-
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="" />
         </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Bojan Golic</h1>
+          <h3>
+            Software Engineer <br/><span><br/>Front End Developer</span>
+          </h3>
+          
+          <h3>
+          <span>Back End Developer</span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
     </div>
-  )
+  );
 }
