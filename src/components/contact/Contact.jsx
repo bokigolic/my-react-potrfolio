70349a853bf10d38be5051b02a9fda15e07acb89
Bojan Golic <bokigolic32@gmail.com>
2022-07-12 20:52:57 -0400

    Contact done, email...

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./contact.scss"
 
 export default function Contact() {
+  const [message, setMessage] = useState(false)
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    setMessage(true)
+    
+  }
   return (
-    <div className="contact" id='contact'></div>
+  
+    <div className="contact" id='contact'>
+      <div className='left'>
+        <img src='assets/shake.svg' alt='shake-hands' />
+      </div>
+      <div className='right'>
+        <h2>Contact</h2>
+        <form onSubmit={handleSubmit}>
+          <input type='text' placeholder='Email' />
+          <textarea placeholder='Message'></textarea>
+          <button type='submit'>Sent</button>
+          {message && <span>Thanks, I will replay ASAP</span>}
+        </form>
+      </div>
+    </div>
   )
 }
--- src/components/testmonials/Tstimonials.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import "./testimonials.scss"
-
-export default function Tstimonials() {
-  return (
-    <div className="testimonials" id='testimonials'></div>
-  )
-}
